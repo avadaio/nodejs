8ad9a72417c3c801c2605070dc4b547c1f579611
Thomas <anhnt@mageplaza.com>
2021-09-14 15:48:28 +0700

    Init class with typing file init

---
+++ types/index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for avadaio-node
+// Project: avadaio-node
+// Definitions by: Thomas Nguyen <anhnt@avada.io>
+
+/*~ This is the module template file for class modules.
+ *~ You should rename it to index.d.ts and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+
+/*~ Note that ES6 modules cannot directly export class objects.
+ *~ This file should be imported using the CommonJS-style:
+ *~   import x = require('someLibrary');
+ *~
+ *~ Refer to the documentation to understand common
+ *~ workarounds for this limitation of ES6 modules.
+ */
+
+/*~ This declaration specifies that the class constructor function
+ *~ is the exported object from the file
+ */
+
+export = AvadaEmailMarketing;
+
+/*~ Write your module's methods and properties in this class */
+declare class AvadaEmailMarketing {
+
+}
