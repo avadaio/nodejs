f31ede89c38494d4eb4d6cda8af6fa59abcf2c48
Thomas <anhnt@mageplaza.com>
2021-09-15 20:21:50 +0700

    Update test snippet

--- __test__/test_order_bulk.js
+++ __test__/test_order_bulk.js
@@ -1,9 +1,9 @@
-const AvadaEmailMarketing = require('../src');
+const AVADA = require('../dist');
 
 (async () => {
   try {
     /** @type AvadaEmailMarketing */
-    const avadaio = new AvadaEmailMarketing({
+    const avadaio = new AVADA({
       appId: 'tTg4lFZkpV6vH74n6UB6',
       apiKey: 'd3af7f191829062d877871d4b28c3445'
     });
