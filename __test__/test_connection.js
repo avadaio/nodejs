fa329ccf88ec3d15317c3393981152361266be8e
Thomas <anhnt@mageplaza.com>
2021-09-15 17:38:05 +0700

    Update transpilation. Update dependency.

---
+++ __test__/test_connection.js
@@ -0,0 +1,15 @@
+const AvadaEmailMarketing = require('../dist');
+
+(async () => {
+    try {
+        /** @type AvadaEmailMarketing */
+        const avadaio = new AvadaEmailMarketing({
+            appId: 'tTg4lFZkpV6vH74n6UB6',
+            apiKey: 'd3af7f191829062d877871d4b28c3445'
+        });
+        const resp = await avadaio.connection.test();
+        console.log(resp);
+    } catch (e) {
+        console.error(e);
+    }
+})();
