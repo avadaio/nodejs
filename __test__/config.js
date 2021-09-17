74a2ae9a280986e9ce2d4f3a98b0c561999e47a0
Thomas <anhnt@mageplaza.com>
2021-09-17 10:09:02 +0700

    Update config

--- __test__/config.js
+++ __test__/config.js
@@ -4,8 +4,8 @@
  * @type {{apiKey: string, appId: string}}
  */
 const config = {
-  appId: 'tTg4lFZkpV6vH74n6UB6',
-  apiKey: 'd3af7f191829062d877871d4b28c3445'
+  appId: '[YOUR_APP_ID]',
+  apiKey: '[YOUR_API_KEY]'
 };
 
 module.exports = config;
